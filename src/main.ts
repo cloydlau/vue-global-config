ee3ac2f595fb735804a730f58cf895d8d7e74419
cloydlau <253055734@qq.com>
2022-04-11 01:35:37 +0800

    wip

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import classifyGlobalConfig from './classifyGlobalConfig'
 import listenGlobalHooks from './listenGlobalHooks'
 import { evaluateListeners, evaluateAttrs } from './evaluateListeners'
+import evaluateProp from './evaluateProp'
 
 export {
   classifyGlobalConfig,
   listenGlobalHooks,
   evaluateListeners,
-  evaluateAttrs
+  evaluateAttrs,
+  evaluateProp
 }
