90193b504a7a82d343ee3de67b38662f93d701ff
cloydlau <253055734@qq.com>
2022-08-17 09:56:24 +0800

    workflow: add case-police

--- demo/vue2.7/components/TheWelcome.vue
+++ demo/vue2.7/components/TheWelcome.vue
@@ -28,7 +28,7 @@ import SupportIcon from './icons/IconSupport.vue'
 
       This project is served and bundled with
       <a href="https://vitejs.dev/guide/features.html" target="_blank">Vite</a>. The recommended IDE
-      setup is <a href="https://code.visualstudio.com/" target="_blank">VSCode</a> +
+      setup is <a href="https://code.visualstudio.com/" target="_blank">VS Code</a> +
       <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>. If you need to
       test your components and web pages, check out
       <a href="https://www.cypress.io/" target="_blank">Cypress</a> and
